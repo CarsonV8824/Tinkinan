62bda7aa61e43b1b6ce350c3638a1353aa0698a7
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2025-12-31 12:32:26 -0600

    Update canvas.py

--- src/canvas.py
+++ src/canvas.py
@@ -5,13 +5,16 @@ import math
 
 class Canvas:
 
+    
     def __init__(self, root: ttk.Frame):
         self.root = root
         self.canvas = tk.Canvas(self.root, height=650, width=650)
         self.canvas.pack(expand=True, fill="both", anchor="center")
-        self.__draw_hexagon(325,325, 50)
+        self.hexagons = []  
+        
+        self.__draw_hexagon(325, 325, 75)
 
-    def __draw_hexagon(self,x:int, y:int, size:float | int):
+    def __draw_hexagon(self, x: int, y: int, size: float | int):
         points = []
         for i in range(6):
             angle_deg = 60 * i - 30
@@ -19,4 +22,5 @@ class Canvas:
             px = x + size * math.cos(angle_rad)
             py = y + size * math.sin(angle_rad)
             points.extend([px, py])
-        self.canvas.create_polygon(points, outline='black', fill='', width=2)
+        hex_id = self.canvas.create_polygon(points, outline='black', fill='', width=2)
+        self.hexagons.append(hex_id)  # Step 2: Store the hexagon ID
